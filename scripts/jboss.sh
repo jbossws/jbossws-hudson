128cb57e72671756aca736df20edfc481d2e95ee
richard.opalka@jboss.com <richard.opalka@jboss.com>
2011-02-10 09:31:35 +0000

    adding CXF AS7 core job

--- scripts/jboss.sh
+++ scripts/jboss.sh
@@ -3,10 +3,19 @@
 PROGNAME=`basename $0`
 DIRNAME=`dirname $0`
 JBOSS_HOME="$1"
-BINDADDR="$3"
 CMD="$2"
+BINDADDR="$3"
 
 export JBOSS_HOME
+PIDFILE="$JBOSS_HOME/bin/jboss.pid"
+
+if [ -f "$JBOSS_HOME/bin/run.sh" ]; then
+   RUN_CMD="$DIRNAME/runjboss.sh -b $BINDADDR"
+else
+   RUN_CMD="$JBOSS_HOME/bin/standalone.sh"
+   export LAUNCH_JBOSS_IN_BACKGROUND="true"
+   export JBOSS_PIDFILE=$PIDFILE
+fi
 
 #
 # Helper to complain.
@@ -15,22 +24,15 @@ warn() {
    echo "$PROGNAME: $*"
 }
 
-if [ ! -f "$JBOSS_HOME/bin/run.sh" ]; then
-   warn "Cannot find: $JBOSS_HOME/bin/run.sh"
-   exit 1
-fi
-
 case "$CMD" in
 start)
     # This version of run.sh obtains the pid of the JVM and saves it as jboss.pid
     # It relies on bash specific features
-    # Do you want to hide jboss output?
-    /bin/bash $DIRNAME/runjboss.sh -b $BINDADDR &
+    /bin/bash $RUN_CMD &
     ;;
 stop)
-    pidfile="$JBOSS_HOME/bin/jboss.pid"
-    if [ -f "$pidfile" ]; then
-       pid=`cat "$pidfile"`
+    if [ -f "$PIDFILE" ]; then
+       pid=`cat "$PIDFILE"`
        echo "kill pid: $pid"
        kill $pid
        if [ "$?" -eq 0 ]; then
@@ -38,10 +40,9 @@ stop)
          sleep 20
          kill -9 $pid &> /dev/null
        fi
-       rm "$pidfile"
+       rm "$PIDFILE"
     else
-       warn "No pid found, using shutdown"
-       $JBOSS_HOME/bin/shutdown.sh -S > /dev/null &
+       warn "No pid found!"
     fi
     ;;
 restart)
@@ -49,5 +50,6 @@ restart)
     $0 start
     ;;
 *)
-    echo "usage: $0 jboss_instance (start|stop|restart|help)"
+    echo "usage: $0 JBOSS_HOME (start|stop|restart|help)"
 esac
+
