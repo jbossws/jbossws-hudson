5bf4596d89d9542360995f6dece2624c9abb85ff
asoldano <asoldano>
2015-02-10 13:34:33 +0000

    svn merge -r 19144:HEAD https://svn.jboss.org/repos/jbossws/hudson/branches/arquillian .

--- scripts/jbossws-qa.sh
+++ scripts/jbossws-qa.sh
@@ -7,14 +7,6 @@ ensureJavaExists() {
   fi
 }
 
-setupJBossHome() {
-  rm -rf $WORKSPACE/jboss-as
-  cp -r $JBOSS_INSTANCE $WORKSPACE/jboss-as
-  export JBOSS_HOME=$WORKSPACE/jboss-as
-  echo "Overwriting default application-users.properties / application-roles.properties on AS..."
-  cp $STACK_DIR/etc/application-*.properties $JBOSS_HOME/standalone/configuration
-}
-
 setupEnv() {
   TMP_DIR_PROPERTY=
   if [ -d /data/tmp ]; then
@@ -25,112 +17,25 @@ setupEnv() {
     echo "Cannot detect temporary directory";
     exit -1;
   fi
-
-  ENVIRONMENT="$TMP_DIR_PROPERTY -Dmaven.opts=-U -Djboss.bind.address=$JBOSS_BIND_ADDRESS -Djbossws.integration.target=$JBOSS_TARGET -D$JBOSS_TARGET.home=$JBOSS_HOME"
-}
-
-stopJBoss() {
-  $SCRIPTS_DIR/jboss.sh $JBOSS_HOME stop $JBOSS_BIND_ADDRESS
-}
-
-startJBoss() {
-  $SCRIPTS_DIR/jboss.sh $JBOSS_HOME start $JBOSS_BIND_ADDRESS
-}
-
-copyJBossLogs() {
-  if [ -e $JBOSS_HOME/standalone/log/boot.log ]; then
-    cp $JBOSS_HOME/standalone/log/boot.log $WORKSPACE/jboss-boot.log
+  if [ "$JBOSS_INSTANCE" != "" ]; then
+    rm -rf $WORKSPACE/jboss-as
+    cp -r $JBOSS_INSTANCE $WORKSPACE/jboss-as
+    export JBOSS_HOME=$WORKSPACE/jboss-as
+    ENVIRONMENT="$TMP_DIR_PROPERTY -Dmaven.opts=-U -Dserver.home=$JBOSS_HOME"
+  else
+    ENVIRONMENT="$TMP_DIR_PROPERTY -Dmaven.opts=-U"
   fi
-  cp $JBOSS_HOME/standalone/log/server.log $WORKSPACE/jboss-server.log
+}
+
+logMavenDependencies() {
+  cd $STACK_DIR
+  mvn -Ptestsuite,spring,dist clean
+  mvn -P$JBOSS_TARGET dependency:tree | tee $WORKSPACE/dependency-tree.txt
 }
 
 copyTestLogs() {
   cat $WORKSPACE/tests.log | egrep FIXME\|FAILED | sort -u | tee $WORKSPACE/fixme.txt
   cat $STACK_DIR/modules/dist/target/exclude-file/test-excludes-$JBOSS_TARGET.txt $WORKSPACE/fixme.txt | egrep "\[\S*]" > $WORKSPACE/errata-$JBOSS_TARGET.txt
-}
-
-removeJBossLogs() {
-  rm -f $JBOSS_HOME/standalone/log/boot.log
-  rm -f $JBOSS_HOME/standalone/log/server.log
-}
-
-ensureRunningJBoss() {
-    $SCRIPTS_DIR/http-spider.sh $JBOSS_BIND_ADDRESS:8080 $WORKSPACE
-    if [ -e $WORKSPACE/spider.failed ]; then
-      tail -n 100 $JBOSS_HOME/standalone/log/server.log
-      stopJBoss
-      copyJBossLogs
-      exit 1
-    fi
-#    sleep 5 
-}
-
-logMavenDependencies() {
-  mvn -Ptestsuite,$JBOSS_TARGET dependency:tree | tee $WORKSPACE/dependency-tree.txt
-}
-
-deployCoreDistributionWithSpring() {
-  cd $STACK_DIR
-  mvn -Ptestsuite,dist,spring clean
-  ant $ENVIRONMENT -Dspring=true deploy-$JBOSS_TARGET
-}
-
-deployCoreDistribution() {
-  cd $STACK_DIR
-  mvn -Ptestsuite,dist clean
-  ant $ENVIRONMENT deploy-$JBOSS_TARGET
-}
-
-deployBinaryDistributionWithSpring() {
-  cd $STACK_DIR
-  mvn -Ptestsuite,dist clean
-  ant $ENVIRONMENT build-bin-dist
-  cd target
-  rm -rf jbossws-$STACK_ID-bin-dist
-  unzip jbossws-$STACK_ID-bin-dist.zip
-  cd jbossws-$STACK_ID-bin-dist
-  cp ant.properties.example ant.properties
-  ant $ENVIRONMENT -Dspring=true deploy-$JBOSS_TARGET
-}
-
-deployBinaryDistribution() {
-  cd $STACK_DIR
-  mvn -Ptestsuite,dist clean
-  ant $ENVIRONMENT build-bin-dist
-  cd target
-  rm -rf jbossws-$STACK_ID-bin-dist
-  unzip jbossws-$STACK_ID-bin-dist.zip
-  cd jbossws-$STACK_ID-bin-dist
-  cp ant.properties.example ant.properties
-  ant $ENVIRONMENT deploy-$JBOSS_TARGET
-}
-
-redeployBinaryDistribution() {
-  cd $STACK_DIR/target/jbossws-$STACK_ID-bin-dist
-  ant clean
-  ant $ENVIRONMENT deploy-$JBOSS_TARGET
-}
-
-deploySourceDistributionWithSpring() {
-  cd $STACK_DIR
-  mvn -Ptestsuite,dist,spring clean
-  ant $ENVIRONMENT build-src-dist
-  cd target
-  rm -rf jbossws-$STACK_ID-src-dist
-  unzip jbossws-$STACK_ID-src-dist.zip
-  cd jbossws-$STACK_ID-src-dist
-  ant $ENVIRONMENT -Dspring=true deploy-$JBOSS_TARGET
-}
-
-deploySourceDistribution() {
-  cd $STACK_DIR
-  mvn -Ptestsuite,dist clean
-  ant $ENVIRONMENT build-src-dist
-  cd target
-  rm -rf jbossws-$STACK_ID-src-dist
-  unzip jbossws-$STACK_ID-src-dist.zip
-  cd jbossws-$STACK_ID-src-dist
-  ant $ENVIRONMENT deploy-$JBOSS_TARGET
 }
 
 detectFailures() {
@@ -143,123 +48,30 @@ detectFailures() {
 }
 
 runTestsViaMavenWithSpring() {
-  mvn $ENVIRONMENT -Ptestsuite,hudson,spring,$JBOSS_TARGET $TEST_OPTS integration-test 2>&1 | tee $WORKSPACE/tests.log
+  mvn $ENVIRONMENT -Phudson,spring,$JBOSS_TARGET $TEST_OPTS integration-test 2>&1 | tee $WORKSPACE/tests.log
 }
 
 runTestsViaMaven() {
-  mvn $ENVIRONMENT -Ptestsuite,hudson,$JBOSS_TARGET $TEST_OPTS integration-test 2>&1 | tee $WORKSPACE/tests.log
-}
-
-runTestsViaAnt() {
-  ant $ENVIRONMENT tests-clean tests $TEST_OPTS 2>&1 | tee $WORKSPACE/tests.log
-}
-
-addTestQueue() {
-  if [ "$JBOSS_TARGET" != "jboss710" ] && [ "$JBOSS_TARGET" != "jboss711" ]; then
-    sleep 10
-    $JBOSS_HOME/bin/jboss-cli.sh -c command="/subsystem=messaging/hornetq-server=default/jms-queue=testQueue/:add(entries=[\"queue/test\",\"java:jboss/exported/jms/queue/test\"])"
-  fi
+  mvn $ENVIRONMENT -Phudson,$JBOSS_TARGET $TEST_OPTS integration-test 2>&1 | tee $WORKSPACE/tests.log
 }
 
 coreTestWithSpring() {
-  setupJBossHome
   setupEnv
   ensureJavaExists
-  stopJBoss
-  deployCoreDistributionWithSpring
-  removeJBossLogs
-  startJBoss
-  ensureRunningJBoss
-  addTestQueue
+#  addTestQueue TODO
   logMavenDependencies
   runTestsViaMavenWithSpring
   copyTestLogs
-  stopJBoss
-  copyJBossLogs
   detectFailures
 }
 
 coreTest() {
-  setupJBossHome
   setupEnv
   ensureJavaExists
-  stopJBoss
-  deployCoreDistribution
-  removeJBossLogs
-  startJBoss
-  ensureRunningJBoss
   logMavenDependencies
   runTestsViaMaven
   copyTestLogs
-  stopJBoss
-  copyJBossLogs
   detectFailures
 }
 
-binaryDistributionTestWithSpring() {
-  setupJBossHome
-  setupEnv
-  ensureJavaExists
-  stopJBoss
-  deployBinaryDistributionWithSpring
-  removeJBossLogs
-  startJBoss
-  ensureRunningJBoss
-  addTestQueue
-  runTestsViaAnt
-  copyTestLogs
-  stopJBoss
-  copyJBossLogs
-  detectFailures
-}
 
-binaryDistributionTest() {
-  setupJBossHome
-  setupEnv
-  ensureJavaExists
-  stopJBoss
-  deployBinaryDistribution
-  removeJBossLogs
-  startJBoss
-  ensureRunningJBoss
-  runTestsViaAnt
-  copyTestLogs
-  stopJBoss
-  copyJBossLogs
-  detectFailures
-}
-
-sourceDistributionTestWithSpring() {
-  setupJBossHome
-  setupEnv
-  ensureJavaExists
-  stopJBoss
-  deploySourceDistributionWithSpring
-  removeJBossLogs
-  startJBoss
-  ensureRunningJBoss
-  addTestQueue
-  logMavenDependencies
-  runTestsViaMavenWithSpring
-  copyTestLogs
-  stopJBoss
-  copyJBossLogs
-  detectFailures
-}
-
-sourceDistributionTest() {
-  setupJBossHome
-  setupEnv
-  ensureJavaExists
-  stopJBoss
-  deploySourceDistribution
-  removeJBossLogs
-  startJBoss
-  ensureRunningJBoss
-  logMavenDependencies
-  runTestsViaMaven
-  copyTestLogs
-  stopJBoss
-  copyJBossLogs
-  detectFailures
-}
